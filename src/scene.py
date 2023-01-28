03fdb198d14585bdab434523a658e95b9ba2bc72
Jordan BRION <jrd.brion@gmail.com>
2023-01-28 14:01:06 +0100

    scene.py: add attribute for list of drawables

--- src/scene.py
+++ src/scene.py
@@ -1,3 +1,7 @@
 class Scene:
     def __init__(self) -> None:
-        self.background_color = []
+        self.__background_color = []
+        self.__drawables = []
+
+    def add_drawable(self, drawable) -> None:
+        self.__drawables.append(drawable)
