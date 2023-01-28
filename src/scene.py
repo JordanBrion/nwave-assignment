d60d2a60c162997b9d279b7067f4efcd5296935f
Jordan BRION <jrd.brion@gmail.com>
2023-01-28 14:06:42 +0100

    scene.py: overload ctor with background_color

--- src/scene.py
+++ src/scene.py
@@ -1,6 +1,6 @@
 class Scene:
-    def __init__(self) -> None:
-        self.__background_color = []
+    def __init__(self, background_color) -> None:
+        self.__background_color = background_color
         self.__drawables = []
 
     def add_drawable(self, drawable) -> None:
