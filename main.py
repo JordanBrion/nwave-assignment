9ffe4627df8f5641f181667858422f836e612ba3
Jordan BRION <jrd.brion@gmail.com>
2023-02-14 21:36:57 +0100

    main.py: render in 2 files within the same run

--- main.py
+++ main.py
@@ -12,7 +12,13 @@ def main():
     scene.add_entity(Disk(radius=radius, position=[-250, -216], color=[0, 255, 0]))
     scene.add_entity(Disk(radius=radius, position=[250, -216], color=[0, 0, 255]))
     
-    viewport = Viewport(width=1100, height=1000, renderer=Renderer(writer=FileWriter("/Users/jordanbrion/Downloads/bonjour.jpg")))
+    jpg_writer = FileWriter("/Users/jordanbrion/Downloads/aaa1.jpg")
+    renderer = Renderer(writer=jpg_writer)
+    viewport = Viewport(width=1100, height=1000, renderer=renderer)
+    viewport.render(scene)
+
+    png_writer = FileWriter("/Users/jordanbrion/Downloads/aaa2.png")
+    renderer.writer = png_writer
     viewport.render(scene)
 
     #JsonSerializer().serialize_scene(scene)
