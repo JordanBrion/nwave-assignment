c0c99659b2dc6934c70066f998114f613349ebab
Jordan BRION <jrd.brion@gmail.com>
2023-02-15 20:31:52 +0100

    test_serialize.py: create parent class for testcase

--- nwave/test_serialize.py
+++ nwave/test_serialize.py
@@ -5,8 +5,7 @@ from .serialize import JsonSerializer, XmlSerializer
 from .scene import Scene
 from .entities import Disk
 
-class Test_Serialize(unittest.TestCase):
-
+class NwaveTestCase(unittest.TestCase):
     def __init__(self, methodName: str = "runTest") -> None:
         super().__init__(methodName)
         self.disk_radius=250
@@ -21,13 +20,16 @@ class Test_Serialize(unittest.TestCase):
     def get_golden_directory(self):
         return os.path.join("nwave", "golden")
 
-    def get_serialize_golden_directory(self):
-        return os.path.join(self.get_golden_directory(), "serialize")        
-
-    def assertWithGoldenFile(self, json, golden_fullpath):
+    def assert_with_golden_file(self, json, golden_fullpath):
         with open(golden_fullpath, "r") as f:
             expected_json = f.read()
             self.assertEqual(json, expected_json)
+class Test_Serialize(NwaveTestCase):
+    def __init__(self, methodName: str = "runTest") -> None:
+        super().__init__(methodName)
+
+    def get_serialize_golden_directory(self):
+        return os.path.join(self.get_golden_directory(), "serialize")        
 
     def test_should_serialize_to_empty_json_when_scene_is_empty(self):
         empty_scene = Scene(background_color=[102, 204, 255])
@@ -39,7 +41,7 @@ class Test_Serialize(unittest.TestCase):
     def test_should_serialize_to_3_disks_in_json_when_scene_has_3_disks(self):
         json = JsonSerializer().serialize_scene(self.make_scene_with_3_disks())
 
-        self.assertWithGoldenFile(
+        self.assert_with_golden_file(
             json, 
             golden_fullpath=os.path.join(self.get_serialize_golden_directory(), "three_disks.json")
         )
@@ -54,7 +56,7 @@ class Test_Serialize(unittest.TestCase):
     def test_should_serialize_to_3_disks_in_xml_when_scene_has_3_disks(self):        
         json = XmlSerializer().serialize_scene(self.make_scene_with_3_disks())
 
-        self.assertWithGoldenFile(
+        self.assert_with_golden_file(
             json, 
             golden_fullpath=os.path.join(self.get_serialize_golden_directory(), "three_disks.xml")
         )
