7a74809f6e8d43605f9de42b45c1b559f86a0d79
Jordan BRION <jrd.brion@gmail.com>
2023-02-15 21:12:20 +0100

    test_should_be_the_same_as_the_golden_png

--- nwave/test_serialize.py
+++ nwave/test_serialize.py
@@ -1,9 +1,16 @@
 import unittest
 import os
+from PIL import Image
+import imagehash
 
 from .serialize import JsonSerializer, XmlSerializer
 from .scene import Scene
 from .entities import Disk
+from .viewport import Viewport
+from .filewriter import FileWriter
+from .renderer import Renderer
+import uuid
+import tempfile
 
 class NwaveTestCase(unittest.TestCase):
     def __init__(self, methodName: str = "runTest") -> None:
@@ -59,4 +66,38 @@ class Test_Serialize(NwaveTestCase):
         self.assert_with_golden_file(
             json, 
             golden_fullpath=os.path.join(self.get_serialize_golden_directory(), "three_disks.xml")
+        )
+
+class Test_Renderer(NwaveTestCase):
+
+    def __init__(self, methodName: str = "runTest") -> None:
+        super().__init__(methodName)
+
+    def get_renderer_golden_directory(self):
+        return os.path.join(self.get_golden_directory(), "renderer")        
+
+    def get_fullpath_temporary_image(self, image_extension):
+        return os.path.join(tempfile.gettempdir(), str(uuid.uuid4()) + image_extension)
+
+    def assert_image_to_golden(self, image_fullpath, golden_fullpath):
+        _, file_extension = os.path.splitext(image_fullpath)
+        _, golden_file_extension = os.path.splitext(golden_fullpath)
+
+        self.assertEqual(file_extension, golden_file_extension)
+
+        with Image.open(image_fullpath) as image:
+            with Image.open(golden_fullpath) as golden_image:
+                image_hash = imagehash.average_hash(golden_image)
+                golden_hash = imagehash.average_hash(image)
+                self.assertEqual(image_hash, golden_hash)
+
+    def test_should_be_the_same_as_the_golden_png(self):
+        png_writer = FileWriter(self.get_fullpath_temporary_image(".png"))
+        renderer = Renderer(writer=png_writer)
+        viewport = Viewport(width=1100, height=1000, renderer=renderer)
+        viewport.render(self.make_scene_with_3_disks())
+
+        self.assert_image_to_golden(
+            png_writer.fullpath, 
+            os.path.join(self.get_renderer_golden_directory(), "three_disks.png")
         )
