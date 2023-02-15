4e28b25ce9e072feb59fbc8943f5bb124e77acda
Jordan BRION <jrd.brion@gmail.com>
2023-02-15 21:28:18 +0100

    entities.py: add HOWTO for new entity

--- nwave/entities.py
+++ nwave/entities.py
@@ -1,3 +1,10 @@
+# The Disk entity is displayed within the Viewport.
+# If you want to add a new entity:
+# 1/ define the new class and its attributes
+# 2/ override render() + serialize() methods
+# 3/ add the corresponding methods within the renderer and serialize classes
+# 4/ at that point, you should be able to add instances of that new entity within the Scene
+
 class Disk:
     def __init__(self, radius, position, color) -> None:
         self.radius = radius
